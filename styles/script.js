9b0a057650fe23b0fdc22e3067c0ddcfb20671d5
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-01-28 07:52:37 +0000

    Declared variables for current weather & userinput

--- styles/script.js
+++ styles/script.js
@@ -6,3 +6,17 @@ var currentWeatherURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&l
 
 // Here we are building the URL we need to query the 5 day weather forecast database for different cities
 var fivedayForecastURL = "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey
+
+
+// Declaring the variables for the user input in the (Document Object Model) elements
+var searchCity = document.getElementById("search-form")
+var searchBtn = document.getElementById("search-button")
+
+// Declaring the variables for the current weather data card in the DOM elements
+var currentWeather = document.getElementById("weather-today")
+var cityName = document.getElementById("city-name")
+var todaysDate = document.querySelector(".current-date")
+var weatherIcon = document.querySelector(".weather-icon")
+var temperature = document.querySelector(".temperature")
+var windSpeed = document.querySelector(".wind-speed")
+var humidity = document.querySelector(".humidity")
