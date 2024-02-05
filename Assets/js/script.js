13b4ccce31970eade346f3d9e1f14fdb183fbd6e
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-02-05 01:49:50 +0000

    Fixing file errors in html for css and javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,13 +3,11 @@ var APIKey = "5acf52002dc0d24c5d94eabd6ebffa10";
 
 // Here we are building the URL we need to query the current weather database for different cities
 var currentWeatherURL =
-  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" +
-  APIKey;
+  "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" + APIKey;
 
 // Here we are building the URL we need to query the 5 day weather forecast database for different cities
 var fivedayForecastURL =
-  "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" +
-  APIKey;
+  "http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=" + APIKey;
 
 // Declaring the variables for the user input in the (Document Object Model) elements
 var searchCity = document.getElementById("search-input");
